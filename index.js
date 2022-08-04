4a982ddb115aa64adb53c4799ac10513859f42a6
Aya Ali <1aya.dn.ali@gmail.com>
2022-08-04 13:46:26 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+
+
+      function clickAlert() {
+          alert('I was clicked!'); 
+      }
+    input.addEventListener('click', clickAlert);
+
+
+    
 }
+      
+      
+      
